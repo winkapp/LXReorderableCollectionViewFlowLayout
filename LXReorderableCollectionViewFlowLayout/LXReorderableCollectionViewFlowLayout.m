3662c1b8652c99c46c2ed882f8d2c1e68b7e4171
Adam Cumiskey <adam@winkapp.com>
2015-09-22 16:20:22 -0400

    Revert "fixing scroll on ios9"

--- LXReorderableCollectionViewFlowLayout/LXReorderableCollectionViewFlowLayout.m
+++ LXReorderableCollectionViewFlowLayout/LXReorderableCollectionViewFlowLayout.m
@@ -95,28 +95,25 @@ static NSString * const kLXCollectionViewKeyPath = @"collectionView";
 }
 
 - (void)setupCollectionView {
-    if (![self.collectionView.gestureRecognizers containsObject:_longPressGestureRecognizer]) {
-        _longPressGestureRecognizer = [[UILongPressGestureRecognizer alloc] initWithTarget:self
-                                                                                    action:@selector(handleLongPressGesture:)];
-        _longPressGestureRecognizer.minimumPressDuration = self.longPressDuration;
-        _longPressGestureRecognizer.delegate = self;
-        
-        // Links the default long press gesture recognizer to the custom long press gesture recognizer we are creating now
-        // by enforcing failure dependency so that they doesn't clash.
-        for (UIGestureRecognizer *gestureRecognizer in self.collectionView.gestureRecognizers) {
-            if ([gestureRecognizer isKindOfClass:[UILongPressGestureRecognizer class]]) {
-                [gestureRecognizer requireGestureRecognizerToFail:_longPressGestureRecognizer];
-            }
+    _longPressGestureRecognizer = [[UILongPressGestureRecognizer alloc] initWithTarget:self
+                                                                                action:@selector(handleLongPressGesture:)];
+    _longPressGestureRecognizer.minimumPressDuration = self.longPressDuration;
+    _longPressGestureRecognizer.delegate = self;
+    
+    // Links the default long press gesture recognizer to the custom long press gesture recognizer we are creating now
+    // by enforcing failure dependency so that they doesn't clash.
+    for (UIGestureRecognizer *gestureRecognizer in self.collectionView.gestureRecognizers) {
+        if ([gestureRecognizer isKindOfClass:[UILongPressGestureRecognizer class]]) {
+            [gestureRecognizer requireGestureRecognizerToFail:_longPressGestureRecognizer];
         }
-        [self.collectionView addGestureRecognizer:_longPressGestureRecognizer];
     }
-
-    if (![self.collectionView.gestureRecognizers containsObject:_panGestureRecognizer]) {
-        _panGestureRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self
-                                                                        action:@selector(handlePanGesture:)];
-        _panGestureRecognizer.delegate = self;
-        [self.collectionView addGestureRecognizer:_panGestureRecognizer];
-    }
+    
+    [self.collectionView addGestureRecognizer:_longPressGestureRecognizer];
+    
+    _panGestureRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self
+                                                                    action:@selector(handlePanGesture:)];
+    _panGestureRecognizer.delegate = self;
+    [self.collectionView addGestureRecognizer:_panGestureRecognizer];
     
     // Useful in multiple scenarios: one common scenario being when the Notification Center drawer is pulled down
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleApplicationWillResignActive:) name: UIApplicationWillResignActiveNotification object:nil];
